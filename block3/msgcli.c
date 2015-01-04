972a66363392921b6337d81da68ebdb94f9c5f83
danvop <danvop@gmail.com>
2015-01-04 07:57:22 +0300

    forking with snd/rcv conversation complited

--- block3/msgcli.c
+++ block3/msgcli.c
@@ -3,15 +3,30 @@
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#include <strings.h>
+#include <string.h>
 #include "msgtypes.h"
+
+int msgid;
+int i;
+struct msg_t message;
+char buf[MAXLEN];
+
+void send_message(char * s){
+	size_t len=strlen(s);
+	if (len>MAXLEN) len=MAXLEN-1;
+	message.mtype = 2;
+	message.snd_pid = getpid();
+	memset(message.body,0,MAXLEN);
+	strncpy(message.body, s, len);
+	msgsnd(msgid, &message, sizeof(message) - sizeof(message.mtype), 0); // посылаем ответ
+}
 
 int main(int argc, char * argv[])
 {
-  int msgid;
-  int i;
-  struct msg_t message;
-  char buf[MAXLEN];
+	char * hello_server = "Hello, server!\n";
+	char * i_m_ok = "I am OK. Thanks!\n";
+	char * request = "How are you?\n";
+	char * bye_server = "Bye, server!\n";
   msgid = msgget(KEY, 0666);  // получаем идентификатор очереди
   if (msgid == -1)  // Очереди не существует
   {
@@ -19,14 +34,22 @@ int main(int argc, char * argv[])
      return EXIT_FAILURE;
   }
   i = 0;
-  while ( (i < (MAXLEN - 1)) && ((message.body[i++] = getchar()) !=  '\n') );
-  message.body[i] = '\0';
-  message.mtype = 2;
-  message.snd_pid = getpid ();
-  msgsnd(msgid, &message, sizeof(message) - sizeof(message.mtype), 0);  // посылаем сообщение
+  send_message(hello_server);
   msgrcv(msgid, &message, sizeof(message) - sizeof(message.mtype), 1, 0);  // ждем ответа
   printf("Server (pid= %i) responded: %s\n", message.snd_pid, message.body);
-  message.mtype = 2;
-  msgsnd(msgid, &message, sizeof(message), 0);  // посылаем подтверждение
+  
+  send_message(i_m_ok);
+  msgrcv(msgid, &message, sizeof(message) - sizeof(message.mtype), 1, 0);  // ждем ответа
+  printf("Server (pid= %i) responded: %s\n", message.snd_pid, message.body);
+
+  send_message(request);
+  msgrcv(msgid, &message, sizeof(message) - sizeof(message.mtype), 1, 0);  // ждем ответа
+  printf("Server (pid= %i) responded: %s\n", message.snd_pid, message.body);
+  
+	send_message(bye_server);
+  msgrcv(msgid, &message, sizeof(message) - sizeof(message.mtype), 1, 0);  // ждем ответа
+  printf("Server (pid= %i) responded: %s\n", message.snd_pid, message.body);
+  
+	msgsnd(msgid, &message, sizeof(message), 0);  // посылаем подтверждение
   return EXIT_SUCCESS;
 }
