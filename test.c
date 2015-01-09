aa6a2d741e725e152b6d816c8ac16a353de83c53
danvop <danvop@gmail.com>
2015-01-09 06:23:55 +0300

    block8 added, block7 modified

--- test.c
+++ test.c
@@ -1,4 +1,4 @@
-
+#include <stdio.h>
 
 
 #define MAXLEN 10
@@ -10,8 +10,13 @@ int main(){
 	int array1[MAXLEN]={1,2,3,4,5,6,7,8,9,10};
 	int array2[MAXLEN]={0};
 	int i = 0;
+	int * p;
 	
 	for (i = 0; i < (MAXLEN/2); i++)
 		array2[i]=array1[MAXLEN-1-i];
-
+	
+	p = array2;
+	while(*p) printf("%d ", *p++);
+	
+	printf("\n");
 }
